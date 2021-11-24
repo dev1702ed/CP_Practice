a5b1e23d3d4b7fc7fb9076b18d648b5fcaf6b763
dev1702ed <d.bhirin@iitg.ac.in>
2021-11-24 11:56:48 +0530

    optimised

--- cses/counting_towers.cpp
+++ cses/counting_towers.cpp
@@ -3,13 +3,14 @@
 #define M 1000000007
 using namespace std;
  
-int main(){
+ int n=1000000;
+ ll dp[1000001][2];
+ 
+ int main(){
     
-    int t;
-    cin>>t;
-    while(t--){
-      int n;cin>>n;
-      ll dp[n+2][2];
+ 
+    
+      
       dp[n+1][0] = dp[n+1][1] = 1;
       for(int i=n;i >= 2;i--){
           ll o1 = (dp[i+1][1]+dp[i+1][0])%M;
@@ -20,10 +21,15 @@ int main(){
           dp[i][0] = (o1 + o2+ o3)%M;
           dp[i][1] = (o1 + o4)%M;
       }
-       
-       cout<< (dp[2][1] + dp[2][0])%M <<endl;
-   
 
-    }
+
+      int t;
+      cin>>t;
+      while(t--){
+       int q;cin>>q;
+       cout<< (dp[1000000-q+2][1] + dp[1000000-q+2][0])%M <<endl;
+      }
+
+    
    return 0;
 }
